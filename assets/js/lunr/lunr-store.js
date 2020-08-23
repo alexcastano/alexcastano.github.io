c91235589b9875db119844dced5d263407309caa
Deployment bot (from Travis CI) <alexcastanodev@gmail.com>
2020-08-23 17:02:18 +0000

    Deploy blog to github.com/alexcastano/alexcastano.github.io.git:master

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Hello World",
+        "excerpt":"It’s finally here!! This is my new and shiny blog!! Hooray :tada: :tada: :tada: It has been a long wait… but better late than never. The main topic of this small space in Internet will be programming and technical stuff. But I don’t consider myself only a developer because what...","categories": [],
+        "tags": ["blog"],
+        "url": "https://alexcastano.com/hello-world/",
+        "teaser": "https://alexcastano.com/assets/images/sunrise.jpg"
+      },{
+        "title": "Start using Docker",
+        "excerpt":"Docker can be an overwhelming technology at the beginning. However we don’t need to use it in every piece of our developments, even less when we are learning it. We can start using slowly. Use docker for databases in a development environment I think a good first step to start...","categories": [],
+        "tags": ["docker"],
+        "url": "https://alexcastano.com/start-using-docker/",
+        "teaser": null
+      },{
+        "title": "Github Pages with Jekyll Plugins and Theme",
+        "excerpt":"Github allows us to host a Jekyll page using Github Pages, but the support for plugins and themes is very limited. We cannot freely use the plugins that you want, and we have exactly the same problem with the themes. Only safe plugins and the next available themes can be...","categories": [],
+        "tags": ["blog","continuous integration"],
+        "url": "https://alexcastano.com/github-pages-with-jekyll-plugins-and-themes/",
+        "teaser": null
+      },{
+        "title": "Username generation in Ruby on Rails",
+        "excerpt":"Today we are going to create a unique username automatically using the user’s full name in a typical Ruby on Rails application, i.e., \"Alex Castaño\" =&gt; \"alex_castano\" It can be very useful when creating a sign-up form, as we want it to be as simple as possible, like our Deverify...","categories": [],
+        "tags": ["ruby on rails","ruby"],
+        "url": "https://alexcastano.com/generate-unique-usernames-for-ruby-on-rails/",
+        "teaser": "https://alexcastano.com/assets/images/name_label.jpg"
+      },{
+        "title": "The hidden cost of the invisible queries in Rails",
+        "excerpt":"Ruby on Rails is awesome, it is really awesome. It allows us to create web applications without knowing every involved technology. After all, Rails is optimized for programmers happiness and it does a terrific job. One of those technologies where Rails is really helpful is SQL. It removes the complexity...","categories": [],
+        "tags": ["ruby on rails","ruby"],
+        "url": "https://alexcastano.com/the-hidden-cost-of-the-invisible-queries-in-rails/",
+        "teaser": "https://alexcastano.com/assets/images/hidden_cat.jpg"
+      },{
+        "title": "Everything you should know about Ruby Splats",
+        "excerpt":"I’m working on a new project playing with new kids in town: Rails 5.1, Webpack(er), Bootstrap 4, React, etc. I was working with forms when I realized that Bootstrap 4 needs a lot of conditional CSS class to show states. Concatenating strings in a view it does not produce a...","categories": [],
+        "tags": ["ruby"],
+        "url": "https://alexcastano.com/everything-about-ruby-splats/",
+        "teaser": "https://alexcastano.com/assets/images/splat_bazinga.jpg"
+      },{
+        "title": "MoodleNet: more than a technical project",
+        "excerpt":"As many of you already know, I have been hired by Moodle to work on the MoodleNet project. Moodle is the world’s most popular learning platform with more than 120 million users around the world. It is used by many universities on every continent. In fact, I used it myself...","categories": [],
+        "tags": ["MoodleNet","ActivityPub"],
+        "url": "https://alexcastano.com/moodlenet-more-than-a-technical-project/",
+        "teaser": "https://alexcastano.com/assets/images/moodlenet_more_technical_project/live_work_create.jpg"
+      },{
+        "title": "What is ActivityPub?",
+        "excerpt":"This was my first question when I joined to MoodleNet team. Now, after studying it deeply for a few weeks, I feel able to give a proper answer. So I thought of writing a blog post, with my goal being to offer a nice introduction to ActivityPub for technical people....","categories": [],
+        "tags": ["MoodleNet","ActivityPub"],
+        "url": "https://alexcastano.com/what-is-activity-pub/",
+        "teaser": "https://alexcastano.com/assets/images/what_is_activity_pub/activitypub.png"
+      },{
+        "title": "Why and how a GenServer stops?",
+        "excerpt":"The last day, I was creating a GenServer and I realized that I didn’t know the proper way to stop it. I had a vague idea about the process, but I needed fine-grained details. This post is the result of my little research about it. I think this is a...","categories": [],
+        "tags": ["Elixir"],
+        "url": "https://alexcastano.com/how-to-stop-a-genserver-in-elixir/",
+        "teaser": null
+      }]
